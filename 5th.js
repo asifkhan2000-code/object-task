fcfd32630d09e0a54808fc091a767f04e49d6ef8
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-13 04:33:09 +0600

    5th task done

---
+++ 5th.js
@@ -0,0 +1,15 @@
+let myObject = {
+    name: "John Doe",
+    age: 25,
+    city: "Example City",
+    isStudent: true,
+};
+// 1st method
+const keys = Object.keys(myObject);
+for(const key of keys) {
+    console.log("Key:", key, "| type:", typeof myObject[key])
+}
+// 2nd method
+for (const properties in myObject) {
+    console.log("Key:", properties, "| type:",typeof myObject[properties]);
+}
