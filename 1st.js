2c38a54c0320cf7c36b8ac074b32803d88a822e4
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-13 04:13:06 +0600

    1st task done

---
+++ 1st.js
@@ -0,0 +1,9 @@
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": "#daa520",
+};
+var green = "green";
+console.log(colors[green]);
+console.log(colors["golden rod"]);
