343a9e7a42b6b66893a089097a4758baac371e03
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-14 18:27:41 +0600

    all done

--- 5th.js
+++ 5th.js
@@ -1,16 +1,25 @@
 let myObject = {
     name: "John Doe",
     age: 25,
-    city: "Example City",
+    city: "New York city",
     isStudent: true,
 };
-// 1st method
 const keys = Object.keys(myObject);
 const values = Object.values(myObject);
-for(const key of keys) {
+// 1st method
+for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
     console.log("Key:", key, "| type:", typeof myObject[key]);
 }
-// 2nd method
+// 2nd mathod
+for (const key of keys) {
+    console.log("Key:", key, "| type:", typeof myObject[key]);
+}
+// 3rd method
 for (const properties in myObject) {
-    console.log("Key:", properties, "| type:",typeof myObject[properties]);
+    console.log("Key:", properties, "| type:", typeof myObject[properties]);
+}
+//4th method
+for (let i = 0; i < keys.length; i++) {
+    console.log("Key:", keys[i], "| type:", typeof values[i]);
 }
